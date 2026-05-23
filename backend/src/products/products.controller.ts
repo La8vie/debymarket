298e17246bd8567a571d8bc93f20c9a9c17dc3cf
christian <akougael3@gmail.com>
2026-05-23 19:54:41 +0000

    Désactivation temporaire de la sécurité sur la création de produit

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  // Route PRIVÉE (Sécurité désactivée temporairement pour les tests)
   @Post()
+  // @UseGuards(AuthGuard('jwt'), RolesGuard)
+  // @Roles('admin', 'manager') 
   create(@Body() body: any) {
     return this.productsService.create(body);
   }
